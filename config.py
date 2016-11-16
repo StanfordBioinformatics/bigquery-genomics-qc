284cf6cbeafef80da3ee58395abec4714062ac6a
Greg <gregorymcinnes@gmail.com>
2016-11-15 19:24:45 -0800

    Bug fixes and updates

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 class Config(object):
     QUERY_REPO = './mvp_aaa_codelabs/sql'
-    VARIANT_TABLE = 'gbsc-gcp-project-mvp:va_aaa_pilot_data.aaa_20160709_genome_calls_no_qc'
-    EXPANDED_TABLE = 'gbsc-gcp-project-mvp:va_aaa_pilot_data.aaa_20160715_multisample_variants_vqsr_pass'
+    VARIANT_TABLE = 'gbsc-gcp-project-mvp:va_aaa_pilot_data.aaa_genome_calls_sample_qc_v3_1'
+    EXPANDED_TABLE = 'gbsc-gcp-project-mvp:va_aaa_pilot_data.aaa_multisample_variants_sample_qc_v3_1'
     GENOTYPING_TABLE = 'gbsc-gcp-project-mvp:va_aaa_pilot_data.genotyping_data'
     BLACKLISTED_TABLE = 'gbsc-gcp-project-mvp:resources.blacklisted_positions'
     PATIENT_INFO = 'gbsc-gcp-project-mvp:va_aaa_pilot_data.patient_info'
-    QC_DATASET = 'va_aaa_pilot_qc'
+    QC_DATASET = 'va_aaa_pilot_qc_2'
     CLIENT_SECRETS = '/Users/gmcinnes/bin/google_genomics/client_secrets.json'
     PROJECT_NUMBER = 963911152157
     PROJECT_NAME = 'gbsc-gcp-project-mvp'
-    DATASET = 14165412073904006532
+    DATASET = 11134829862918767427
